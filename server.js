b7cf5811180e92f6a6cfbf898b83b66acad7d23a
Nimrod Flaschner <nimrodifla@gmail.com>
2022-03-08 19:05:17 +0200

    init

---
+++ server.js
@@ -0,0 +1,17 @@
+const NSEL = require('nsel');
+const fs = require('fs');
+
+const app = NSEL.app;
+NSEL.listen();
+
+app.get('/words', (req, res)=>{
+    let word_list = String(fs.readFileSync(__dirname + '/words.txt'));
+    while (word_list.includes("'"))
+        word_list = word_list.replace("'", '"');
+    word_list = JSON.parse(word_list);
+    res.send(word_list);
+})
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html');
+});
