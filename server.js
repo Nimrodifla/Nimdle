1e0b16950f5f24d569c68d720d775ed14e1a8836
Nimrod Flaschner <nimrodifla@gmail.com>
2022-03-09 16:05:04 +0200

    more words. can now backspace

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const fs = require('fs');
 const app = NSEL.app;
 NSEL.listen();
 
-app.get('/words', (req, res)=>{
-    let word_list = String(fs.readFileSync(__dirname + '/words.txt'));
+const words = parseWords(__dirname + '/words.txt');
+console.log(words.length + " words");
+
+function parseWords(path) {
+    let word_list = String(fs.readFileSync(path));
     while (word_list.includes("'"))
         word_list = word_list.replace("'", '"');
     word_list = JSON.parse(word_list);
-    res.send({'count': word_list.length, 'words': word_list});
+    return word_list;
+}
+
+app.get('/words', (req, res)=>{
+    res.send({'count': words.length, 'words': words});
 })
 
 app.get('/', (req, res)=>{
--- words_generator.py
+++ words_generator.py
@@ -6,7 +6,6 @@ def addToText(url):
     mybytes = fp.read()
     huge_text += mybytes.decode("utf8")
     fp.close()
-    print("#", end='')
 
 links = ["https://he.wikipedia.org/wiki/%D7%9B%D7%93%D7%95%D7%A8_%D7%94%D7%90%D7%A8%D7%A5",
          "https://he.wikipedia.org/wiki/%D7%91%D7%A2%D7%9C%D7%99_%D7%97%D7%99%D7%99%D7%9D",
@@ -16,7 +15,11 @@ links = ["https://he.wikipedia.org/wiki/%D7%9B%D7%93%D7%95%D7%A8_%D7%94%D7%90%D7
          "https://he.wikipedia.org/wiki/%D7%93%D7%99%D7%90%D7%9C%D7%95%D7%92",
          "https://he.wikipedia.org/wiki/%D7%A1%D7%9C%D7%99%D7%97%D7%94",
          "https://he.wikipedia.org/wiki/%D7%A8%D7%92%D7%A9",
-         "https://he.wikipedia.org/wiki/%D7%A1%D7%9C%D7%A0%D7%92"
+         "https://he.wikipedia.org/wiki/%D7%A1%D7%9C%D7%A0%D7%92",
+         "https://www.adamtsair.co.il/%D7%A1%D7%99%D7%A4%D7%95%D7%A8%D7%99%D7%9D-%D7%9C%D7%99%D7%9C%D7%93%D7%99%D7%9D/",
+         "https://www.adamtsair.co.il/%d7%9e%d6%b4%d7%99%d7%95%d6%b9%d7%9e%d6%b8%d7%a0%d7%95%d6%b9-%d7%a9%d7%81%d6%b6%d7%9c-%d7%97%d6%b2%d7%aa%d7%95%d6%bc%d7%9c-%d7%96%d6%b6%d7%91%d6%b6%d7%9c/",
+         "https://www.adamtsair.co.il/%d7%90%d6%b7%d7%a8%d6%b0%d7%9e%d7%95%d6%b9%d7%9f-%d7%94%d6%b7%d7%98%d6%b7%d7%91%d6%bc%d6%b7%d7%a2%d6%b7%d7%aa/",
+         "https://he.wikipedia.org/wiki/%D7%99%D7%A9%D7%A8%D7%90%D7%9C"
          ]
 
 huge_text = ""
@@ -31,17 +34,22 @@ words = []
 
 def getWords(text):
     copy = text
+    step = int(len(text) / 10)
+    c = 0
     for let in text:
+        if ((c + 1) % step) == 0:
+            print(str((c+1)/step) + ' %')
         if (let in heb_letters) == False:
             copy = copy.replace(let, '')
+        c+=1
     print('Replaced All')
 
     words_text = copy.split(' ')
-    step = len(words_text) / 10
+    step = int(len(words_text) / 10)
     c = 0
     for w in words_text:
-        if (c + 1) % step:
-            print('10 %')
+        if ((c + 1) % step) == 0:
+            print(str((c+1)/step) + ' %')
         if (len(w) == 5) and ((w in words) == False):
             words.append(w)
         c+=1
